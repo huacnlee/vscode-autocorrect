d3af51c8498b40cd2c2ed2ca50127dfd5adeba78
Jason Lee <huacnlee@gmail.com>
2021-06-30 18:27:48 +0800

    Change autocorrect.formatOnSave default false.
    Add auto-correct.format command for manual execute

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,39 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+
+function formatDocument(document: vscode.TextDocument) {
+  const config = vscode.workspace.getConfiguration("auto-correct");
+
+  const cmdPath = config["path"] || "autocorrect";
+  const exec = require("child_process").exec;
+  exec(cmdPath + " --fix " + document.fileName, (err: Error) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(ctx: vscode.ExtensionContext) {
-	ctx.subscriptions.push(vscode.workspace.onDidSaveTextDocument((document) => {
-    const formatOnSave =
-        vscode.workspace.getConfiguration('auto-correct')['formatOnSave'] || false;
-		
-    if (formatOnSave) {
-      const cmdPath =
-          vscode.workspace.getConfiguration('auto-correct')['path'] || 'autocorrect';
-      const exec = require('child_process').exec;
-      exec(cmdPath + ' --fix ' + document.fileName, (err: Error) => {
-        if (err) {
-          console.log(err);
-        }
-      });
-    }
-  }));
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand("auto-correct.format", () => {
+      const document = vscode.window.activeTextEditor?.document;
+      if (document) {
+        formatDocument(document);
+      }
+    })
+  );
+
+  ctx.subscriptions.push(
+    vscode.workspace.onDidSaveTextDocument((document) => {
+      const config = vscode.workspace.getConfiguration("auto-correct");
+      if (config["formatOnSave"]) {
+        formatDocument(document);
+      }
+    })
+  );
 }
 
 // this method is called when your extension is deactivated
